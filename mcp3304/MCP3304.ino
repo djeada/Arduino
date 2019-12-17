074a0f02ce497d95c189b59978730267a7fd60b1
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-12-17 09:33:42 +0000

    quick updates for two channels

--- mcp3304/MCP3304.ino
+++ mcp3304/MCP3304.ino
@@ -1,14 +1,23 @@
-#define DATA_MAX 600
+#define DATA_MAX 500
 #include <SPI.h>
 #include <Arduino.h>
 #include <avr/io.h>
 
-int table[DATA_MAX] = {0};
+int table1[DATA_MAX] = {0};
+int table2[DATA_MAX] = {0};
 
-int readAdc(int pin) {      
+int readAdcCH1(){      
   PORTB &= ~(1 << PB6);
-  SPI.transfer( 0x0C | (pin >> 1)); 
-  int adcValue = ((SPI.transfer(pin << 7) & 0x0f) << 8) + SPI.transfer(0x00);
+  SPI.transfer( 0x0C | (0 >> 1)); 
+  int adcValue = ((SPI.transfer(0 << 7) & 0x0f) << 8) + SPI.transfer(0x00);
+  PORTB |= (1 << PB6);
+  return adcValue; 
+}
+
+int readAdcCH2(){      
+  PORTB &= ~(1 << PB6);
+  SPI.transfer( 0x0C | (1 >> 1)); 
+  int adcValue = ((SPI.transfer(1 << 7) & 0x0f) << 8) + SPI.transfer(0x00);
   PORTB |= (1 << PB6);
   return adcValue; 
 }
@@ -25,11 +34,17 @@ void setup() {
 
 void loop() {
   for(int i = 0; i < DATA_MAX; i++){
-    table[i] = readAdc(0);
+    table1[i] = readAdcCH1();
+    table2[i] = readAdcCH2();
   }
   for(int i = 0; i < DATA_MAX; i++){
-    Serial.println(table[i]);
+    Serial.println(table1[i]);
       delay(50);
   }
-  delay(100000);
+  Serial.println("Second part");
+  for(int i = 0; i < DATA_MAX; i++){
+    Serial.println(table2[i]);
+      delay(50);
+  }
+
 }
